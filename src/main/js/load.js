73e5cce53326cd5ee2b7f469235acadca1757afc
Patryk Likus <mlodyasg1@gmail.com>
2020-11-28 13:40:44 +0100

    save project

---
+++ src/main/js/load.js
@@ -0,0 +1,52 @@
+// showProgressIndicator();
+
+function fetchRandomDate() {
+ handleHttp("http://numbersapi.com/random/date");
+}
+function fetchRandomNumber() {
+ handleHttp("http://numbersapi.com/random/math");
+}
+function handleHttp(url) {
+  showProgressIndicator();
+  fetch(url)
+    .then((response) => response.text())
+    .then((text) => setTimeout(() => displayResult(text, true), 500))
+    .catch(handleError);
+}
+function displayResult(result, isSuccess) {
+  const resultEl = document.getElementById("result");
+  resultEl.innerText = "";
+  resultEl.innerHTML = `<span>${result}</span>`;
+
+  if (isSuccess) {
+    changeFetchBtnText("Fetch random date");
+  }
+
+  hideProgressIndicator();
+}
+
+function showProgressIndicator() {
+  const progressIndicator = document.getElementById("progress-indicator");
+  const main = document.getElementById("content");
+
+  main.classList.add("hidden");
+  progressIndicator.classList.remove("hidden");
+}
+
+function hideProgressIndicator() {
+  const progressIndicator = document.getElementById("progress-indicator");
+  const main = document.getElementById("content");
+
+  main.classList.remove("hidden");
+  progressIndicator.classList.add("hidden");
+}
+
+function handleError() {
+  changeFetchBtnText("Retry");
+  displayResult("Something went wrong...", false);
+}
+
+function changeFetchBtnText(text) {
+  const fetchBtn = document.getElementsByClassName("fetchBtn");
+  fetchBtn.innerText = text;
+}
